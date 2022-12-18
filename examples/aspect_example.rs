91be24c2100dd7fe0abde66e5816d675a666bc07
TheEmeraldBee <brightonlcox@gmail.com>
2022-12-18 10:07:43 -0700

    Added a window config to aspect example.

--- examples/aspect_example.rs
+++ examples/aspect_example.rs
@@ -4,13 +4,22 @@ extern crate macroquad_aspect;
 use macroquad::prelude::*;
 use macroquad_aspect::prelude::*;
 
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Aspect Example".to_string(),
+        window_width: 1080,
+        window_height: 720,
+        ..Default::default()
+    }
+}
 
-#[macroquad::main("Test")]
+#[macroquad::main(window_conf)]
 async fn main() {
     let mut window_context = WindowContext::new(
         vec![
         Aspect::new(1920.0, 1080.0),
-        Aspect::new(1680.0, 1050.0)]
+        Aspect::new(1680.0, 1050.0)
+        ]
     );
 
     loop {
