4c25f1cc1b1714b589998db47022fbc047e14585
brightoncox <brightonlcox@gmail.com>
2023-03-20 11:48:41 -0600

    Added Render Target Scaling Capabilities.

--- src/window.rs
+++ src/window.rs
@@ -13,6 +13,7 @@ pub struct WindowContext {
     pub aspects: Aspects,
     /// If true will always take the closest aspect and scale the screen size based off that
     pub forced: bool,
+    pub scale: u32,
 
     pub active_screen_size: Vec2,
 
@@ -33,6 +34,7 @@ impl WindowContext {
             },
             aspects,
             forced: false,
+            scale: 4,
             active_screen_size: vec2(0.0, 0.0),
             last_window_size: vec2(-100.0, -100.0),
             cur_size: Default::default(),
@@ -131,7 +133,7 @@ pub fn draw_window(context: &mut WindowContext) {
         context.screen_bounds.top_left = vec2(screen_width() / 2.0 - context.cur_size.x / 2.0, screen_height() / 2.0 - context.cur_size.y / 2.0);
         context.screen_bounds.bottom_right = context.screen_bounds.top_left.clone() + context.cur_size;
 
-        context.render_target = render_target(context.active_aspect.width as u32 * 12, context.active_aspect.height as u32 * 12);
+        context.render_target = render_target(context.active_aspect.width as u32 * context.scale, context.active_aspect.height as u32 * context.scale);
         context.camera = Camera2D::from_display_rect(Rect::new(0.0, 0.0, context.active_aspect.width, context.active_aspect.height));
         context.camera.render_target = Some(context.render_target);
     }
